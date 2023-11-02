c357b5f048a6d7fce98a4a4461eb81c711a0d94d
MannyMcCullom <m.mccullom11@gmail.com>
2023-11-02 10:49:35 -0400

    Feature 2

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ const min = 1;
 const max = 10;
 const num = 5;
 
-let guess = prompt(`Guess a number from ${min} to ${max}.`);
 
-if (guess >= min && guess <= max) {
-    if (guess == num) {
-        alert('You guessed right.')
+function guessAgain() {
+    let guess = prompt(`Guess a number from ${min} to ${max}.`);
+    if (guess >= min && guess <= max) {
+        if (guess == num) {
+            return alert('You guessed right.');
+        } else {
+            if (guess < num) {
+                alert('You should have guessed higher.');
+            }
+            if (guess > num) {
+                alert('You should have guesses lower.');
+            }
+        }
     } else {
-        if (guess < num) {
-            alert('You should have guessed higher.')
-        }
-        if (guess > num) {
-            alert('You should have guesses lower.')
-        }
+        alert('Not in range.')
     }
-} else {
-    alert('nope')
+    guessAgain();
 }
+
+guessAgain();
